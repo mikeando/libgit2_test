ee3fd281668a4dac0393b06115b51a3cd9bc0754
Michael Anderson <michael.anderson@runwithrobots.com>
2013-09-20 16:34:27 +0800

    Added support for ssh.
    Requires that libssh2 be built and installed, and then libgit2 built.

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ int config_callback( const git_config_entry* entry, void* /* userdata */ ) {
 }
 
 int push_status_callback(const char *ref, const char *msg, void * /* userdata */ ) {
-  std::cout<<ref<<" : "<<msg<<std::endl;
+  std::cout<<ref<<" : "<<(msg?msg:("up-to-date"))<<std::endl;
   return 0;
 }
 
@@ -31,17 +31,55 @@ int cred_callback(
 	const char *url,
 	const char *username_from_url,
 	unsigned int allowed_types,
-	void *payload)
+	void * /*payload*/)
 {
   std::cout<<cred<<std::endl;
   std::cout<<"URL: "<<url<<std::endl;
   std::cout<<"Username: "<<username_from_url<<std::endl;
   std::cout<<"Allowed types: "<<allowed_types<<std::endl;
-  std::cout<<"Enter password: "<<std::flush;
-  std::string password;
-  std::cin>>password;
-  std::cout<<std::endl;
-  return git_cred_userpass_plaintext_new(cred, username_from_url, password.c_str());
+  if(allowed_types==1) {
+    std::cout<<"Enter password: "<<std::flush;
+    std::string password;
+    std::cin>>password;
+    std::cout<<std::endl;
+    return git_cred_userpass_plaintext_new(cred, username_from_url, password.c_str());
+  } else if (allowed_types==3) {
+    std::cout<<"Enter password: "<<std::flush;
+    std::string password;
+    std::cin>>password;
+    return git_cred_ssh_keyfile_passphrase_new(cred, username_from_url, "/Users/michaelanderson/.ssh/id_rsa.pub", "/Users/michaelanderson/.ssh/id_rsa", password.c_str());
+  }
+  return 1;
+}
+
+void push(git_repository * repo, const char* remote_name) {
+  //Lets try to push to origin...
+  git_remote * remote = NULL;
+  check_error( git_remote_load(&remote, repo, remote_name), "getting remote info");
+
+  git_remote_set_cred_acquire_cb(remote, cred_callback, NULL);
+
+
+  git_push * push = NULL;
+  check_error( git_push_new(&push, remote), "creating push object");
+
+  git_push_options push_options = {GIT_PUSH_OPTIONS_VERSION, 0};
+  check_error( git_push_set_options(push, &push_options), "setting push options");
+
+  const char* refspec = "refs/heads/master:refs/heads/master";
+  check_error( git_push_add_refspec(push, refspec), "adding refspec");
+  check_error( git_push_finish(push), "finishing push" );
+ 
+  if(! git_push_unpack_ok(push) ) {
+    std::cout<<"unpach failed..."<<std::endl;
+  }
+
+  check_error( git_push_update_tips(push), "updating tips" );
+
+  check_error( git_push_status_foreach(push, push_status_callback, NULL), "checking status");
+
+
+  git_push_free(push);
 }
 
 int main() {
@@ -63,33 +101,9 @@ int main() {
 
 
   //Lets try to push to origin...
-  git_remote * remote = NULL;
-  check_error( git_remote_load(&remote, repo, "origin"), "getting remote info");
+  push(repo,"origin_ssh");
+  push(repo,"origin");
 
-  git_remote_set_cred_acquire_cb(remote, cred_callback, NULL);
-
-
-  git_push * push = NULL;
-  check_error( git_push_new(&push, remote), "creating push object");
-
-  git_push_options push_options = {GIT_PUSH_OPTIONS_VERSION, 0};
-  check_error( git_push_set_options(push, &push_options), "setting push options");
-
-  char* refspec = "refs/heads/master:refs/heads/master";
-  check_error( git_push_add_refspec(push, refspec), "adding refspec");
-  check_error( git_push_finish(push), "finishing push" );
- 
-  if(! git_push_unpack_ok(push) ) {
-    std::cout<<"unpach failed..."<<std::endl;
-  }
-
-  check_error( git_push_update_tips(push), "updating tips" );
-
-  check_error( git_push_status_foreach(push, push_status_callback, NULL), "checking status");
-
-
-  git_push_free(push);
-
-
+  std::cout<<"DONE..."<<std::endl;
 
 }
